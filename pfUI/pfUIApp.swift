7ab96ba7c994e9a1bada0d99a9d928e24528bbfc
felix <felix.klug@gmx.at>
2023-09-12 23:53:23 +0200

    Initial Commit

---
+++ pfUI/pfUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pfUIApp.swift
+//  pfUI
+//
+//  Created by Felix Klug on 12.09.23.
+//
+
+import SwiftUI
+
+@main
+struct pfUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
